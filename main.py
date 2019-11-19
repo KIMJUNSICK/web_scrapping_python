4ce6d1d6014526cbb9f1281c41c63760a1148f97
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-19 20:23:14 +0900

    connect to url & get all html in website

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
 
+indeed_result = requests.get("https://www.indeed.co.uk/jobs?q=python&limit=50")
 
-asdasd
+print(indeed_result)  # Response 200 => success !
+
+print(indeed_result.text)
+# get all html ! # we need to pick out data we want to get
