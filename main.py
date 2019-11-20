26cb92888b06fba0f13306565b5c2c2ad85a9ec1
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-20 20:21:29 +0900

    Extract indeed pages in site

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from bs4 import BeautifulSoup
 
 indeed_requests = requests.get("https://www.indeed.co.uk/jobs?q=python&limit=50")
 
-print(indeed_requests)
-
 indeed_soup = BeautifulSoup(indeed_requests.text, "html.parser")
 
-print(indeed_soup)
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+pages = pagination.find_all("a")
+
+spans = []
+for page in pages:
+    spans.append(page.find("span"))
+
+print(spans[:-1])  # -1 is first things in reverse
+
