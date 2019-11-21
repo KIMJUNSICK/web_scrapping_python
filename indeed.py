abf3bf5e2a57419b7ea4bef58a714ce8c949462f
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-21 09:36:55 +0900

    Extract company data with strip()
    strip() remove empty space in list

--- indeed.py
+++ indeed.py
@@ -18,10 +18,17 @@ def extract_indeed_page():
 
 
 def extract_indeed_jobs(last_page):
-    for page in range(last_page):
-        result = requests.get(f"{URL}&start={page*LIMIT}")
-        soup = BeautifulSoup(result.text, "html.parser")
-        job_cards = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-        for job_card in job_cards:
-            title = job_card.find("div", {"class": "title"}).find("a")["title"]
-            print(title)
+    # for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    job_cards = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for job_card in job_cards:
+        title = job_card.find("div", {"class": "title"}).find("a")["title"]
+        company = job_card.find("span", {"class": "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            company = str(company_anchor.string)
+        else:
+            company = str(company.string)
+        company = company.strip()  # for removing empty space
+        print(title, company)
